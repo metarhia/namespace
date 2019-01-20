bbc015502ef4e5941331fb024542654203f6a564
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2019-01-20 22:52:36 +0200

    Use prettier for code formatting

--- namespace.js
+++ namespace.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const submodules = [
-  'main',
-].map(path => './lib/' + path).map(require);
+const submodules = ['main'].map(path => './lib/' + path).map(require);
 
 module.exports = Object.assign({}, ...submodules);
